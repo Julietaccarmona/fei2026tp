b4825b8bb6eb31095f2090b953894c9bda0e8c0b
Julieta <julieta_carmona_99@hotmail.com>
2026-06-01 22:39:41 -0300

    API REST profesor. Punto13

--- backend/aulas/urls.py
+++ backend/aulas/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from .views import CarreraMixin
+from .views import CarreraMixin, ProfesorMixinDetail
 
 urlpatterns = [
     path(
         "carreras/",
         CarreraMixin.as_view(),
         name="carreras"
+    ),
+
+    path(
+        "profesor/<int:pk>/",
+        ProfesorMixinDetail.as_view(),
+        name="profesor-detail"
     ),
 ]
--- backend/aulas/views.py
+++ backend/aulas/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import (
     ListModelMixin,
-    CreateModelMixin
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
 )
 
-from .models import Carrera
-from .serializers import CarreraSerializer
+from .models import Carrera, Profesor
+from .serializers import CarreraSerializer, ProfesorSerializer
 
 
 class CarreraMixin(
@@ -22,3 +25,26 @@ class CarreraMixin(
 
     def post(self, request):
         return self.create(request)
+
+
+class ProfesorMixinDetail(
+    GenericAPIView,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+):
+
+    queryset = Profesor.objects.all()
+    serializer_class = ProfesorSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
